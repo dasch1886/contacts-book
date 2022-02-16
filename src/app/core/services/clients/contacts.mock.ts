fe3471b0949ca4c62b59b477454739bd7ec1f853
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-17 00:35:37 +0100

    add mock data

---
+++ src/app/core/services/clients/contacts.mock.ts
@@ -0,0 +1,14 @@
+import {Contact} from "@shared/models/clients/contact.model";
+
+export const Contacts: Contact[] = [
+  {id: '1', nickname: 'Crazywar', name: 'Robert', surname: 'Pietrzak', email: 'robert.pietrzak@gmail.com', phone: '500 300 400', gender: 'Mężczyzna'},
+  {id: '2', nickname: 'RustySilver', name: 'Rafał', surname: 'Przybylski', email: 'rafal.przybylski@gmail.com', phone: '501 301 401', gender: 'Mężczyzna'},
+  {id: '3', nickname: 'OrangeGlade', name: 'Cezary', surname: 'Michalak', email: 'cezary.michalak@gmail.com', phone: '502 302 402', gender: 'Mężczyzna'},
+  {id: '4', nickname: 'MutedNewt', name: 'Jerzy', surname: 'Kołodziej', email: 'jerzy.kolodziej@gmail.com', phone: '503 303 403', gender: 'Mężczyzna'},
+  {id: '5', nickname: 'UglyDuck', name: 'Alojzy', surname: 'Jankowski', email: 'alojzy.jankowski@gmail.com', phone: '504 304 404', gender: 'Mężczyzna'},
+  {id: '6', nickname: 'WillHunting', name: 'Blanka', surname: 'Piotrowska', email: 'blanka.piotrowska@gmail.com', phone: '505 305 405', gender: 'Kobieta'},
+  {id: '7', nickname: 'SirenSong', name: 'Lara', surname: 'Włodarczyk', email: 'lara.wlodarczyk@gmail.com', phone: '506 306 406', gender: 'Kobieta'},
+  {id: '8', nickname: 'BrunsonDid', name: 'Joanna', surname: 'Lewandowska', email: 'joanna.lewandowska@gmail.com', phone: '507 307 407', gender: 'Kobieta'},
+  {id: '9', nickname: 'AttackAttack', name: 'Natalia', surname: 'Malinowska', email: 'natalia.malinowska@gmail.com', phone: '508 308 408', gender: 'Kobieta'},
+  {id: '10', nickname: 'Obtusk', name: 'Urszula', surname: 'Szewczyk', email: 'urszula.szewczyk@gmail.com', phone: '509 309 409', gender: 'Kobieta'},
+];
