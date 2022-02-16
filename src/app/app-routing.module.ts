8580b5008a8c42adc2444b700c32600e66bb8985
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-17 00:34:08 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppRoute} from "./app.route";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: AppRoute.CLIENTS,
+    pathMatch: 'full'
+  },
+  {
+    path: AppRoute.CLIENTS,
+    loadChildren: () => import('./modules/clients/clients.module').then((m) => m.ClientsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
