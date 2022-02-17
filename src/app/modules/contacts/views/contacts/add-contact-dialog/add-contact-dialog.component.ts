fac45816b75407d3c3623adef5401f4cce11e5ff
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-17 22:40:48 +0100

    feat - init component

---
+++ src/app/modules/contacts/views/contacts/add-contact-dialog/add-contact-dialog.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ContactsService} from "@core/services/contacts/contacts.service";
+import {ToastrService} from "ngx-toastr";
+import {Subscription} from "rxjs";
+import {ColumnsConfig} from "@modules/contacts/views/contacts/table/colums.config";
+import {GenderEnum} from "@shared/enums/gender.enum";
+
+@Component({
+  selector: 'app-add-client-dialog',
+  templateUrl: './add-contact-dialog.component.html',
+  styleUrls: ['./add-contact-dialog.component.scss']
+})
+export class AddContactDialogComponent implements OnInit, OnDestroy {
+  fields = ColumnsConfig;
+  genderEnum = GenderEnum;
+  form: FormGroup;
+  subscription = new Subscription();
+
+  constructor(private fb: FormBuilder,
+              private clientService: ContactsService,
+              private toastr: ToastrService) {
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      nickname: ['', [Validators.required, Validators.maxLength(24)]],
+      name: ['', [Validators.required, Validators.maxLength(24)]],
+      surname: ['', [Validators.required, Validators.maxLength(24)]],
+      email: ['', [Validators.required, Validators.maxLength(32)]],
+      phone: ['', [Validators.required, Validators.maxLength(12)]],
+      gender: ['', [Validators.required]],
+    });
+  }
+
+  createContact() {
+    this.subscription.add(
+      this.clientService.createContact(this.form.value).subscribe(
+        () => this.toastr.success('Dodano kontakt!')
+      )
+    );
+  }
+}
