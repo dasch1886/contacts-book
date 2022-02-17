8dbb40b41db91b8b381bda30c0506aa4a9d6b44f
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-17 01:36:19 +0100

    add config for columns

---
+++ src/app/modules/clients/views/clients/table/colums.config.ts
@@ -0,0 +1,30 @@
+export const ColumsConfig = [
+  {
+    key: 'nickname',
+    header: 'pseudonim'
+  },
+  {
+    key: 'name',
+    header: 'imię'
+  },
+  {
+    key: 'surname',
+    header: 'nazwisko'
+  },
+  {
+    key: 'email',
+    header: 'adres email'
+  },
+  {
+    key: 'phone',
+    header: 'numer telefonu'
+  },
+  {
+    key: 'gender',
+    header: 'płeć'
+  },
+  {
+    key: 'actions',
+    header : ''
+  }
+];
