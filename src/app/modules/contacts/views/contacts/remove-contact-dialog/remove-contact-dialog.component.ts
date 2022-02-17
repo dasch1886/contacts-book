3678eee7cd3d1ca10bee865e2f4d0af0736e6879
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-17 22:41:34 +0100

    feat - init component

---
+++ src/app/modules/contacts/views/contacts/remove-contact-dialog/remove-contact-dialog.component.ts
@@ -0,0 +1,47 @@
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {Contact} from "@shared/models/contacts/contact.model";
+import {ColumnsConfig} from "@modules/contacts/views/contacts/table/colums.config";
+import {ContactsService} from "@core/services/contacts/contacts.service";
+import {ToastrService} from "ngx-toastr";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-remove-contact-dialog',
+  templateUrl: './remove-contact-dialog.component.html',
+  styleUrls: ['./remove-contact-dialog.component.scss']
+})
+export class RemoveContactDialogComponent implements OnInit, OnDestroy {
+  fields: {[key: string]: string};
+  subscription = new Subscription();
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { contact: Contact },
+              private clientService: ContactsService,
+              private toastr: ToastrService) {
+  }
+
+  ngOnInit(): void {
+    this.setFields();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  setFields() {
+    this.fields = ColumnsConfig.reduce((acc, el) => {
+      return {
+        ...acc,
+        [el.key]: el.header
+      }
+    }, {});
+  }
+
+  remove() {
+    this.subscription.add(
+      this.clientService.removeContact(this.data.contact.id).subscribe(
+        () => this.toastr.warning('Usunięto kontakt!')
+      )
+    )
+  }
+}
