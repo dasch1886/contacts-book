0c1d20ccdd31848b3ab3c1707bad67cc3c83dd6c
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-17 01:38:25 +0100

    move table to separate component

---
+++ src/app/modules/clients/views/clients/table/clients-table.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ClientsService} from "@core/services/clients/clients.service";
+import {Contact} from "@shared/models/clients/contact.model";
+import {ColumsConfig} from "@modules/clients/views/clients/table/colums.config";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-clients-table',
+  templateUrl: './clients-table.component.html',
+  styleUrls: ['./clients-table.component.scss']
+})
+export class ClientsTableComponent implements OnInit, OnDestroy {
+  displayedColumns = ColumsConfig;
+  columnsKeys: string[];
+  data: Contact[];
+  subscription = new Subscription();
+
+  constructor(private clientService: ClientsService) {
+  }
+
+  ngOnInit(): void {
+    this.columnsKeys = this.displayedColumns.map(el => el.key);
+    this.refreshData();
+  }
+
+  refreshData() {
+    this.subscription.unsubscribe();
+    this.subscription.add(
+      this.clientService.getClients().subscribe((data) => {
+        this.data = data;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
