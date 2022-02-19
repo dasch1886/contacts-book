1cad8748f6f1704aa269ebd30a9c57513c751205
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-19 16:47:01 +0100

    fix - set fields, remove redundant code, close dialog manually after response

--- src/app/modules/contacts/views/contacts/remove-contact-dialog/remove-contact-dialog.component.ts
+++ src/app/modules/contacts/views/contacts/remove-contact-dialog/remove-contact-dialog.component.ts
@@ -1,47 +1,37 @@
-import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Contact} from "@shared/models/contacts/contact.model";
-import {ColumnsConfig} from "@modules/contacts/views/contacts/table/colums.config";
 import {ContactsService} from "@core/services/contacts/contacts.service";
 import {ToastrService} from "ngx-toastr";
-import {Subscription} from "rxjs";
+import {getFields} from "@modules/contacts/views/utils/fields";
+import {finalize} from "rxjs/operators";
 
 @Component({
   selector: 'app-remove-contact-dialog',
   templateUrl: './remove-contact-dialog.component.html',
   styleUrls: ['./remove-contact-dialog.component.scss']
 })
-export class RemoveContactDialogComponent implements OnInit, OnDestroy {
-  fields: {[key: string]: string};
-  subscription = new Subscription();
+export class RemoveContactDialogComponent implements OnInit {
+  fields: {
+    label: string,
+    value: string
+  }[];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { contact: Contact },
+              private ref: MatDialogRef<RemoveContactDialogComponent>,
               private contactsService: ContactsService,
               private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
-    this.setFields();
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  setFields() {
-    this.fields = ColumnsConfig.reduce((acc, el) => {
-      return {
-        ...acc,
-        [el.key]: el.header
-      }
-    }, {});
+    this.fields = getFields(this.data.contact);
   }
 
   remove() {
-    this.subscription.add(
-      this.contactsService.removeContact(this.data.contact.id).subscribe(
-        () => this.toastr.warning('Usunięto kontakt!')
-      )
+    this.contactsService.removeContact(this.data.contact.id)
+      .pipe(finalize(() => this.ref.close(true)))
+      .subscribe(
+      () => this.toastr.warning('Usunięto kontakt!')
     )
   }
 }
