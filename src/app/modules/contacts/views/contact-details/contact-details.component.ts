403ff1a3c25e1ea9b8384da6ff7d8ca702a81444
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-19 16:55:49 +0100

    fix - set fields, remove redundant code

--- src/app/modules/contacts/views/contact-details/contact-details.component.ts
+++ src/app/modules/contacts/views/contact-details/contact-details.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Contact} from "@shared/models/contacts/contact.model";
-import {ColumnsConfig} from "@modules/contacts/views/contacts/table/colums.config";
 import {ContactsRoute} from "@modules/contacts/views/contacts.route";
+import {getFields} from "@modules/contacts/views/utils/fields";
 
 @Component({
   selector: 'app-contact-details',
@@ -10,26 +9,18 @@ import {ContactsRoute} from "@modules/contacts/views/contacts.route";
   styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent implements OnInit {
-  fields: {[key: string]: string};
-  contact: Contact;
+  fields: {
+    label: string,
+    value: string
+  }[];
 
-  constructor(private act: ActivatedRoute,
+  constructor(private activatedRoute: ActivatedRoute,
               private router: Router) { }
 
   ngOnInit(): void {
-    this.setFields();
-    this.act.data.subscribe(({details}) => {
-      this.contact = details;
+    this.activatedRoute.data.subscribe(({details}) => {
+      this.fields = getFields(details);
     });
-  }
-
-  setFields() {
-    this.fields = ColumnsConfig.reduce((acc, el) => {
-      return {
-        ...acc,
-        [el.key]: el.header
-      }
-    }, {});
   }
 
   goToClients() {
