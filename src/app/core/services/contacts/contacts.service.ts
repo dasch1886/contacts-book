76e8b5e79600541450310574a4f32239ca2545a0
dasch1886 <kamil.kopacz09@gmail.com>
2022-02-19 14:15:48 +0100

    feat - add method to edit contact, and delay on getContacts

--- src/app/core/services/contacts/contacts.service.ts
+++ src/app/core/services/contacts/contacts.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Contacts} from "@core/services/contacts/contacts.mock";
-import {Observable, of} from "rxjs";
+import {Observable, of, throwError} from "rxjs";
 import {Contact} from "@shared/models/contacts/contact.model";
+import {delay} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,10 @@ import {Contact} from "@shared/models/contacts/contact.model";
 export class ContactsService {
   private data: Contact[] = Contacts;
 
-  constructor() { }
+  constructor() {}
 
   getContacts(): Observable<Contact[]> {
-    return of(this.data);
+    return of(this.data).pipe(delay(500));
   }
 
   getContactDetails(id: number) {
@@ -23,6 +24,17 @@ export class ContactsService {
     contact.id = this.data.length ? this.data[this.data.length - 1].id + 1 : 0;
     this.data.push(contact);
     return of({});
+  }
+
+  editContact(contact: Contact) {
+    const index = this.data.findIndex((el) => el.id === contact.id);
+
+    if (index) {
+      this.data[index] = contact;
+      return of({});
+    }
+
+    return throwError(() => new Error('Kontakt nie istnieje'));
   }
 
   removeContact(id: number) {
